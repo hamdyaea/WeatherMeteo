8218a91bb92b87feb2d93ec85bbcb192d33c30fe
hamdyaea <hamdy.aea@gmail.com>
2019-03-09 15:58:40 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,52 @@
+![Awesome](awesome.svg) ![Python](python.png)  
+
+# Weather Meteo in Python 3
+
+
+![Screenshot](screenshot3.png)
+
+
+## What's this software ?  
+
+This python 3 software show you a weather report. 
+It use https://wttr.in/ look at https://wttr.in/:help for more information about it.
+
+## Language selection
+
+![Screenshot](screenshot1.png)
+
+## Enter a location
+
+![Screenshot](screenshot2.png)
+
+## What you need to make it work :  
+
+The last version of Python 3.
+
+you also need to have the easygui library for python 3:
+
+At least the version 0.98
+
+```sh
+sudo python3 -m pip install --upgrade easygui 
+```
+You need wget for python 3:
+
+
+```sh
+sudo pip3 install wget
+```
+
+## How to launch this software :  
+
+```sh
+python3 meteo.py
+```  
+
+## Developer - Author
+
+Hamdy Abou El Anein
+
+## Homepage
+
+http://www.daylightlinux.ch 
---
+++ meteo.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+
+# Developer : Hamdy Abou El Anein
+
+# This software use wttr.in, look at https://wttr.in/:help for more information about it.
+# you need the easygui and wget library for Python 3.
+
+
+import wget
+from easygui import *
+
+
+# Weather result
+
+def weather():
+    global city
+    if city == None : #if nothing the default city is Bern
+        city = "Bern"
+    elif city == "":
+        city = "Bern"
+    url = (("http://wttr.in/")+str(city)+str("_Fp_lang=")+str(lng)+str(".png"))
+    filename = wget.download(url,out="meteo.png")
+
+    image = 'meteo 1.png'
+    msg = "This is the weather report for this location"
+    choices = ["Change city"]
+    reply = buttonbox(msg, image=image, choices=choices)
+    if reply == "Change city":
+        location()
+
+# Location selection
+
+def location():
+    global city
+    msg = "Enter a city or a location\n\nBy defautl it's Bern - Switzerland"
+    title = "Location"
+    default = "Bern"
+    city = enterbox(msg,title,default)
+    if city == None : #if nothing the default city is Bern
+        city == "Bern"
+    elif city == "":
+        city = "Bern"
+    weather()
+
+# Language selection
+def language():
+    global lng
+    msg = "What is your language ?"
+    title = "Languages"
+    choices = ["English","German","French","Danish","Persian","Indonesian","Italian","Norwegian Bokmål","Dutch","Russian","Afrikaans"]
+    choice = choicebox(msg, title, choices)
+
+    if choice == "Afrikaans":
+        lng = "af"
+    elif choice == "Danish":
+        lng = "da"
+    elif choice == "German":
+        lng = "de"
+    elif choice == "French":
+        lng = "fr"
+    elif choice == "Persian":
+        lng = "fa"
+    elif choice == "Indonesian":
+        lng = "id"
+    elif choice == "Italian":
+        lng = "it"
+    elif choice == "Norwegian Bokmål":
+        lng = "nb"
+    elif choice == "Dutch":
+        lng = "nl"
+    elif choice == "Russian":
+        lng = "ru"
+    elif choice == None : #if nothing it's english
+        lng = "en"
+    else:
+        lng = "en"  # Default is english
+
+    location()
+
+language()
