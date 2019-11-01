49aefc386b054d3faa24ad034ba69be4470fe6d5
hamdyaea <hamdy.aea@protonmail.com>
2019-11-01 13:12:44 +0100

    code beauty

--- meteo.py
+++ meteo.py
@@ -8,29 +8,40 @@
 
 import wget
 from easygui import *  # sudo pip3 install pillow if needed and easygui
-import unidecode  #sudo pip3 install unidecode
+import unidecode  # sudo pip3 install unidecode
 import os
 
 # Weather result
 
+
 def weather():
     global city
-    if city == None : #if nothing the default city is Bern
+    if city == None:  # if nothing the default city is Bern
         city = "Bern"
     elif city == "":
         city = "Bern"
-    unaccent_city = unidecode.unidecode(city) #remove accent and all non-ascii characters
-    url = (("http://wttr.in/")+str(unaccent_city)+str("_Fp_lang=")+str(lng)+str(".png"))
-    filename = wget.download(url,out="meteo.png")
+    unaccent_city = unidecode.unidecode(
+        city
+    )  # remove accent and all non-ascii characters
+    url = (
+        ("http://wttr.in/")
+        + str(unaccent_city)
+        + str("_Fp_lang=")
+        + str(lng)
+        + str(".png")
+    )
+    filename = wget.download(url, out="meteo.png")
 
-    image = 'meteo.png'
+    image = "meteo.png"
     msg = "This is the weather report for this location"
     choices = ["Change city"]
     reply = buttonbox(msg, image=image, choices=choices)
     if reply == "Change city":
         location()
 
+
 # Location selection
+
 
 def location():
     global city
@@ -44,19 +55,32 @@ def location():
     msg = "Enter a city or a location\n\nBy defautl it's Bern - Switzerland"
     title = "Location"
     default = "Bern"
-    city = enterbox(msg,title,default)
-    if city == None : #if nothing the default city is Bern
+    city = enterbox(msg, title, default)
+    if city == None:  # if nothing the default city is Bern
         city == "Bern"
     elif city == "":
         city = "Bern"
     weather()
+
 
 # Language selection
 def language():
     global lng
     msg = "What is your language ?"
     title = "Languages"
-    choices = ["English","German","French","Danish","Persian","Indonesian","Italian","Norwegian Bokmål","Dutch","Russian","Afrikaans"]
+    choices = [
+        "English",
+        "German",
+        "French",
+        "Danish",
+        "Persian",
+        "Indonesian",
+        "Italian",
+        "Norwegian Bokmål",
+        "Dutch",
+        "Russian",
+        "Afrikaans",
+    ]
     choice = choicebox(msg, title, choices)
 
     if choice == "Afrikaans":
@@ -79,7 +103,7 @@ def language():
         lng = "nl"
     elif choice == "Russian":
         lng = "ru"
-    elif choice == None : #if nothing it's english
+    elif choice == None:  # if nothing it's english
         lng = "en"
     else:
         lng = "en"  # Default is english
